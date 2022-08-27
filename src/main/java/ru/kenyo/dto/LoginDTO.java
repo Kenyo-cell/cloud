4f158d02bf752650fa8d76254d530122dc590963
Kenyo-cell <rekurt3000@gmail.com>
2022-08-27 17:26:50 +0300

    feat: add serialization annotation

--- src/main/java/ru/kenyo/dto/LoginDTO.java
+++ src/main/java/ru/kenyo/dto/LoginDTO.java
@@ -1,3 +1,6 @@
 package ru.kenyo.dto;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public record LoginDTO(String login, String password) { }
--- src/main/java/ru/kenyo/dto/UpdateFilenameDTO.java
+++ src/main/java/ru/kenyo/dto/UpdateFilenameDTO.java
@@ -1,4 +1,7 @@
 package ru.kenyo.dto;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public record UpdateFilenameDTO(String filename) {
 }
