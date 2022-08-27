da1be051a0be6ea2c8ed43be39c365d4fe6de2ea
Kenyo-cell <rekurt3000@gmail.com>
2022-08-27 16:11:26 +0300

    feat: created some service test

---
+++ src/test/java/ru/kenyo/FileServiceTest.java
@@ -0,0 +1,62 @@
+package ru.kenyo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import ru.kenyo.dto.FilePreviewDTO;
+import ru.kenyo.entity.FileEntity;
+import ru.kenyo.exception.BadRequestException;
+import ru.kenyo.repository.FileRepository;
+import ru.kenyo.service.FileService;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@SpringBootTest
+public class FileServiceTest {
+    private static final List<FileEntity> files = Arrays.asList(
+            new FileEntity("test", "content".getBytes()),
+            new FileEntity("neto", "logy".getBytes()),
+            new FileEntity("java", "spring".getBytes()),
+            new FileEntity("kotlin", "ktor".getBytes())
+    );
+
+    @MockBean
+    private FileRepository fileRepository;
+
+    @Autowired
+    private FileService service;
+
+    @Test
+    public void failedUpdateFileTest() {
+        Mockito.when(fileRepository.findById(Mockito.anyString())).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(
+                BadRequestException.class,
+                () -> service.updateFile(files.get(2).getFilename(), "scala")
+        );
+    }
+
+    @Test
+    public void successUpdateFileTest() {
+        Mockito.when(fileRepository.findById("java")).thenReturn(Optional.of(files.get(2)));
+
+        Assertions.assertDoesNotThrow(() -> service.updateFile(files.get(2).getFilename(), "scala"));
+    }
+
+    @Test
+    public void successGetFiles() {
+        Mockito.when(fileRepository.findAll()).thenReturn(files);
+
+        Assertions.assertEquals(service.getFiles(null).size(), files.size());
+    }
+}
